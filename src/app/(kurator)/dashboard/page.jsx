2f4ed7370812ee67d3f0ea0b269000cc0a56d3e5
Mai El-Zein <mai_ez@outlook.dk>
2025-06-13 10:30:52 +0200

    check med vercel?

--- src/app/(kurator)/dashboard/page.jsx
+++ src/app/(kurator)/dashboard/page.jsx
@@ -1,4 +1,6 @@
 import {
+  getArtworkByEventID,
+  getEvent,
   getEventDates,
   getEventLocations,
   getNeededArtworkDataPrEvent,
@@ -7,10 +9,25 @@ import {
 import EventFilterAndList from "@/components/global/filter/EventFilterAndList";
 
 export default async function Dashboard() {
+  //Til Filter
   const eventsDates = await getEventDates();
   const eventsLocations = await getEventLocations();
+
+  //Til Content
+
+  // const eventData = await getEvent();
+  // let artImgsPrEvent = [];
+  // let artWorkData = [];
+  // eventData.map(async (event) => {
+  //   console.log("dashboard: ", event);
+  //    artImgsPrEvent = await Promise.all(
+  //      event.artworksIds.map(async (object_number) => {
+  //        artWorkData = await getArtworkByEventID(object_number);
+  //      })
+  //    );
+  // });
+
   const eventDataWithArtworks = await getNeededArtworkDataPrEvent();
-  //console.log(eventDataWithArtworks);
   return (
     <main>
       <EventFilterAndList
