9b52d9ecba85f8d63c1af4ab81a595087e02f2eb
Mai El-Zein <mai_ez@outlook.dk>
2025-06-17 12:18:32 +0200

    /eventView i middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,6 +3,7 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 const isPublicRoute = createRouteMatcher([
   "/",
   "/events(.*)",
+  "/eventView(.*)",
   "/sign-in(.*)",
   "/sign-up(.*)",
 ]);
